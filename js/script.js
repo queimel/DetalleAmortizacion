2b823a37ed96d87c2f72de1ca3ceb90797b45a1c
ExtremeDreams <davidortegar@icloud.com>
2018-11-27 20:28:12 +0100

    "Se agrega la ultima fila con las sumas de las columnas respectivas"

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 (function () {
+
+    totalColumna1 = 0;
+    totalColumna2 = 0;
+    totalColumna3 = 0;
+    totalColumna4 = 0;
+    totalColumna5 = 0;
+    total = "Total";
+
     // declaramos variables
     var monto, anio, interes, areaMsjes, cuota;
     //asignamos area mensajes en variable
@@ -56,6 +64,7 @@
                             var cell4 = row.insertCell(3);
                             var cell5 = row.insertCell(4);
                             var cell6 = row.insertCell(5);
+                            
                             cell1.innerHTML = index -1;
 
                             if(index === 1){
@@ -67,21 +76,42 @@
                                 var totalAmortizado = cuotaAmortizacion + prevTotAmort;
                                 var capital = prevCapital - cuotaAmortizacion;
                                 prevCapital = capital;
-                                prevTotAmort = totalAmortizado;
 
 
                                 cell2.innerHTML = Math.ceil(cuota);
+                                totalColumna1 = parseFloat(totalColumna1 + Math.ceil(cuota));
                                 cell3.innerHTML = parseFloat(cuotaInteres).toFixed(0);
+                                totalColumna2 = parseFloat(totalColumna2 + Math.ceil(parseFloat(cuotaInteres).toFixed(0)));
                                 cell4.innerHTML = parseFloat(cuotaAmortizacion).toFixed(0);
+                                totalColumna3 = parseFloat(totalColumna3) + Math.ceil(parseFloat(cuotaAmortizacion).toFixed(0));
                                 cell5.innerHTML = parseFloat(totalAmortizado).toFixed(0);
-                                cell6.innerHTML = parseFloat(capital).toFixed(0);
-
-                                
+                                totalColumna4 = parseFloat(totalColumna4 ) +parseFloat(totalAmortizado).toFixed(0);
+                                cell6.innerHTML = parseFloat(capital).toFixed(0);  
+                                totalColumna5 = parseFloat(totalColumna5 + parseFloat(capital).toFixed(0));    
+                    
+                             
                                 
 
                             }
+                            
                         }
+
                     }
+                    f = anios+2;
+                    var row = tabla.insertRow(f);
+                    var cell1 = row.insertCell(0);
+                    var cell2 = row.insertCell(1);
+                    var cell3 = row.insertCell(2);
+                    var cell4 = row.insertCell(3);
+                    var cell5 = row.insertCell(4);
+                    var cell6 = row.insertCell(5);
+                    
+                    cell1.innerHTML = "Total";
+                    cell2.innerHTML = totalColumna1;                
+                    cell3.innerHTML = totalColumna2;
+                    cell4.innerHTML = totalColumna3;
+
+                    
                 }
             }else{
                 areaMsjes.innerHTML = "Debe ingresar un monto númerico";
@@ -91,6 +121,10 @@
         }
     });
 
+    
+
+    
+
 })();
 
 function getCuota(prestamo, interes, anios){
@@ -99,11 +133,13 @@ function getCuota(prestamo, interes, anios){
     var calculo3 = 1-(calculo2);
     return calculo1 / calculo3;
 }
-
-/* Queda:
- - Agregar totales
- - eliminar filas de tabla cuando da click a btn consultar
- */
+/* Agregar totales */
+function SumarColumna(grilla, columna) {
+ 
+    
+    
+}   
+/* eliminar filas de tabla cuando da click a btn consultar */
 function limpiar() {
 
     var tableHeaderRowCount = 1;
